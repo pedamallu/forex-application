50a2765d01e935741ee38986b00813d2492fdddf
hemanthtpg <hemanthtpg@gmail.com>
2022-11-17 19:46:14 +0530

    Added project files

---
+++ src/main/java/com/forexservice/ForexApplicationTests.java
@@ -0,0 +1,11 @@
+package com.forexservice;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ForexApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
